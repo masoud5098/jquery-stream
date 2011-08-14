60bf2b8317f5815a5d458589ae80d4e56b1f3337
flowersinthesand <flowersinthesand@d3f0bfe9-4edb-0784-f560-4b5892850fe9>
2011-08-14 06:16:01 +0000

    commented

--- sub-projects/jquery-stream-atmosphere/trunk/src/main/webapp/jquery.stream.atmosphere.js
+++ sub-projects/jquery-stream-atmosphere/trunk/src/main/webapp/jquery.stream.atmosphere.js
@@ -1,12 +1,37 @@
-// Configurations for Atmosphere
+/*
+ * Atmosphere plugin for jQuery Stream
+ * http://atmosphere.java.net/
+ * http://code.google.com/p/jquery-stream/
+ * 
+ * Copyright 2011, Donghwan Kim 
+ * Licensed under the Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Compatible with jQuery Stream 1.2+
+ */
 $.stream.setup({
+	
+	// Atmosphere runs on any Java based web server maintaining session using JSESSIONID cookie
+	// and sets 'Access-Control-Allow-Origin' header to '*' unless 'enableAccessControl' option is not false
 	enableXDR: true,
+	
+	// Atmosphere always prints junk padding wisely and it end with <!-- EOD -->
+	// and there is no identifier concept
 	handleOpen: function(text, message) {
+		// message.index = text.indexOf("<!-- EOD -->") + "<!-- EOD -->".length;
 		message.index = text.indexOf("<!-- EOD -->") + 12;
 	},
+	
+	// This plugin assumes that you can send message in accord with the default message format
+	// if not possible, you can parse the response chunks directly by implementing the following handler
+	// handleMessage: function(text, message, stream) {},
+	
+	// Atmosphere doesn't need metadata
 	handleSend: function(type) {
+		// Because all except send-type request will be canceled
 		if (type !== "send") {
 			return false;
 		}
 	}
+	
 });
