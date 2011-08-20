81b833844d2b6edd94cf76ce8411022b8ba44665
flowersinthesand <flowersinthesand@d3f0bfe9-4edb-0784-f560-4b5892850fe9>
2011-08-20 11:22:59 +0000

    wrapped the plugin in a closure

--- sub-projects/jquery-stream-atmosphere/trunk/src/main/webapp/jquery.stream.atmosphere.js
+++ sub-projects/jquery-stream-atmosphere/trunk/src/main/webapp/jquery.stream.atmosphere.js
@@ -9,30 +9,35 @@
  *
  * Compatible with jQuery Stream 1.2+
  */
-$.stream.setup({
+(function($) {
 	
-	// Atmosphere runs on any Java based web server maintaining session using JSESSIONID cookie
-	// and sets 'Access-Control-Allow-Origin' header to '*' unless 'enableAccessControl' option is not false
-	enableXDR: true,
-	
-	// Atmosphere always prints junk padding ending with <!-- EOD --> wisely
-	// and there is no identifier concept
-	handleOpen: function(text, message) {
-		// 'Content-Type' header of the response must be set to 'text/plain'
-		// message.index = text.indexOf("<!-- EOD -->") + "<!-- EOD -->".length;
-		message.index = text.indexOf("<!-- EOD -->") + 12;
-	},
-	
-	// This plugin assumes that you can send message in accord with the default message format
-	// if not possible, you can parse the response chunks directly by implementing the following handler
-	// handleMessage: function(text, message, stream) {},
-	
-	// Atmosphere doesn't need metadata
-	handleSend: function(type) {
-		// All except send-type request will be canceled
-		if (type !== "send") {
-			return false;
+	// Default configurations
+	$.stream.setup({
+		
+		// Atmosphere runs on any Java based web server maintaining session using JSESSIONID cookie
+		// and sets 'Access-Control-Allow-Origin' header to '*' unless 'enableAccessControl' option is not false
+		enableXDR: true,
+		
+		// Atmosphere always prints junk padding ending with <!-- EOD --> wisely
+		// and there is no identifier concept
+		handleOpen: function(text, message) {
+			// 'Content-Type' header of the response must be set to 'text/plain'
+			// message.index = text.indexOf("<!-- EOD -->") + "<!-- EOD -->".length;
+			message.index = text.indexOf("<!-- EOD -->") + 12;
+		},
+		
+		// This plugin assumes that you can send message in accord with the default message format
+		// if not possible, you can parse the response chunks directly by implementing the following handler
+		// handleMessage: function(text, message, stream) {},
+		
+		// Atmosphere doesn't need metadata
+		handleSend: function(type) {
+			// All except send-type request will be canceled
+			if (type !== "send") {
+				return false;
+			}
 		}
-	}
+		
+	});
 	
-});
+})(jQuery);
